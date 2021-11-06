82e1b76a151db1898edb0abe5b40343689f7b543
jiangsheng <437822838@qq.com>
2021-11-05 20:11:07 -0400

    modified the rdma-setup script

--- scripts/rdma-setup.sh
+++ scripts/rdma-setup.sh
@@ -4,8 +4,29 @@
 
 # install required packages
 sudo apt update
-sudo apt-get install libmlx4-1 infiniband-diags ibutils ibverbs-utils rdmacm-utils perftest \
-  rdma-core mstflint libibverbs-dev librdmacm-dev
+sudo apt install -y rdma-core perftest
+#sudo apt-get install -y libmlx4-1 infiniband-diags ibutils ibverbs-utils rdmacm-utils perftest \
+#  rdma-core mstflint libibverbs-dev librdmacm-dev
 
-# verify the PCIe device is connected to the adapter card
-lspci | grep Mellanox
+# install the OFED driver
+# first clean the unnecessary drivers from various providers
+sudo rm -rf /etc/libibverbs.d/*
+# then download the package and install
+cd /mnt || exit   # recommend to mount a large-size device here
+wget mellanox.com/downloads/ofed/MLNX_OFED-4.9-4.0.8.0/MLNX_OFED_LINUX-4.9-4.0.8.0-ubuntu18.04-x86_64.tgz
+tar -xzvf MLNX_OFED_LINUX-4.9-4.0.8.0-ubuntu18.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.9-4.0.8.0-ubuntu18.04-x86_64/ || exit
+sudo ./mlnxofedinstall ---without-fw-update
+# remove modules
+sudo rmmod rpcrdma
+sudo rmmod rdma_cm
+# then restart service
+sudo /etc/init.d/openibd restart
+
+
+# verify the rdma device
+ibv_devices
+
+# at last, it's suggested to reboot the machine
+# sudo reboot
+# sudo mount /dev/nvme0n1p4 /mnt
