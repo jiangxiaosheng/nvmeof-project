43a0a76c34c733442334a28f89978178728b47c9
jiangsheng <437822838@qq.com>
2021-12-07 17:27:17 -0500

    added benchmark scripts

--- scripts/fio/randrw.sh
+++ scripts/fio/randrw.sh
@@ -11,10 +11,9 @@ for jobs in $(seq 1 $max_jobs)
 do
   echo "================== start $jobs jobs ======================="
 
-  fio --cpus_allowed_policy=split --rw=randrw --direct=1 --ioengine=libaio \
+  fio --cpus_allowed_policy=split --rw=randrw --rwmixread=70 --direct=1 --ioengine=libaio \
   --size=1G --gtod_reduce=0 --thread --group_reporting --time_based --runtime=60 --bs=4k --iodepth=128 \
   --cpus_allowed=0-15 --numjobs="$jobs" --name=random_read --filename=/dev/nvme1n1 --output "$results_dir"/randrw."$jobs".txt
 
   echo "==========================================================="
 done
-
